69dc4ff3f7083d48a23a8cb560a2982e34eec042
kan25d5 <yoshikan2260@gmail.com>
2022-08-15 17:58:33 +0900

    dataset / dataloader完成

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
-from torch.utils.data import DataLoader
-from dataloader.twitter_dataset import TwitterDataset
 from vocab.twitter_vocab import TwitterVocab
-from utilities.training_functions import get_corpus
 
-X, y = get_corpus()
 vocab = TwitterVocab()
 vocab.load_char2id_pkl()
-dataset = TwitterDataset(X, y, vocab)
 
-for i in range(10):
-    print(dataset[i])
 
-dataloader = DataLoader(dataset, batch_size=10, collate_fn=dataset.collate_fn)
-for x, y in dataloader:
+from utilities.training_functions import get_corpus, get_dataset, get_dataloader
+
+X, y = get_corpus()
+all_dataset = get_dataset(X, y, vocab)
+all_dataloader = get_dataloader(all_dataset)
+train_dataloader = all_dataloader[0]
+
+for x, y in train_dataloader:
     print(x)
     print(y)
     break
--- utilities/training_functions.py
+++ utilities/training_functions.py
@@ -6,3 +6,46 @@ def get_corpus(sentiment_type: str = "normal"):
     filepath = f"assets/{sentiment_type}.json"
     corpus = load_json(filepath)
     return corpus["X"], corpus["y"]
+
+
+def get_dataset(X, y, vocab, train_size=0.8, val_size=0.7, maxlen=140, transform=None):
+    from sklearn.model_selection import train_test_split
+    from dataloader.twitter_dataset import TwitterDataset
+
+    X_train, X_other, y_train, y_other = train_test_split(X, y, train_size=train_size)
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_other, y_other, train_size=val_size
+    )
+
+    train_dataset = TwitterDataset(X_train, y_train, vocab, transform, maxlen)
+    val_dataset = TwitterDataset(X_val, y_val, vocab, transform, maxlen)
+    test_dataset = TwitterDataset(X_test, y_test, vocab, transform, maxlen)
+    all_dataset = [train_dataset, val_dataset, test_dataset]
+
+    return all_dataset
+
+
+def get_dataloader(all_dataset, batch_size=100):
+    from torch.utils.data import DataLoader
+
+    train_dataloader = DataLoader(
+        all_dataset[0],
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=all_dataset[0].collate_fn,
+    )
+    val_dataloader = DataLoader(
+        all_dataset[1],
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=all_dataset[1].collate_fn,
+    )
+    test_dataloader = DataLoader(
+        all_dataset[2],
+        batch_size=1,
+        shuffle=False,
+        collate_fn=all_dataset[2].collate_fn,
+    )
+    all_dataloader = [train_dataloader, val_dataloader, test_dataloader]
+    return all_dataloader
+
