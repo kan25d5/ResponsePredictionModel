4d5ec9832d85a328c17b2bdf6db2b407f16c233c
kan25d5 <yoshikan2260@gmail.com>
2022-08-19 19:58:29 +0900

    datasetをpkl化してロード・セーブする機能追加

--- dataloader/twitter_dataset.py
+++ dataloader/twitter_dataset.py
@@ -1,23 +1,27 @@
+import os
+import dill
 import torch
-from typing import List
 from torch.utils.data import Dataset
-from vocab.twitter_vocab import TwitterVocab
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 class TwitterDataset(Dataset):
-    def __init__(self, messages, responses, maxlen=80) -> None:
+    def __init__(self, messages=None, responses=None, maxlen=80) -> None:
         self.messages = messages
         self.responses = responses
         self.maxlen = maxlen
 
-        err_msg = "発話リストと応答リストのサイズが一致しない．"
-        assert len(self.messages) == len(self.responses), err_msg
+        if messages is not None and responses is not None:
+            err_msg = "発話リストと応答リストのサイズが一致しない．"
+            assert len(self.messages) == len(self.responses), err_msg
 
     def __len__(self):
         return len(self.messages)
 
     def __getitem__(self, index: int):
+        if self.messages is None or self.responses is None:
+            err_msg = "発話／応答リストがセットされていません．"
+            raise ValueError(err_msg)
         if index >= len(self.messages):
             raise StopIteration
 
@@ -25,6 +29,20 @@ class TwitterDataset(Dataset):
         res = self.responses[index]
 
         return {"source": msg, "target": res}
+
+    def load_dataset_pkl(self, name: str, folder="assets"):
+        filepath = os.path.join(folder, name)
+        with open(filepath, "rb") as f:
+            self.messages = dill.load(f)
+            self.responses = dill.load(f)
+            self.maxlen = dill.load(f)
+
+    def save_dataset_pkl(self, name: str, folder="assets"):
+        filepath = os.path.join(folder, name)
+        with open(filepath, "wb") as f:
+            dill.dump(self.messages, f)
+            dill.dump(self.responses, f)
+            dill.dump(self.maxlen, f)
 
     def collate_fn(self, batch):
         X = [item["source"] for item in batch]
--- utilities/training_functions.py
+++ utilities/training_functions.py
@@ -3,7 +3,12 @@ from utilities.utility_functions import load_json
 from vocab.twitter_vocab import TwitterVocab
 
 
-def get_corpus(sentiment_type: str = "normal", maxlen=80):
+def get_corpus(sentiment_type: str = "normal", maxlen=80, transform=None):
+    """ 
+    コーパスをロードして発話／応答のリストを返す．\n
+    return X:List[str], y:List[str]
+    """
+
     X, y = [], []
     filepath = f"assets/{sentiment_type}.json"
     corpus = load_json(filepath)
@@ -11,28 +16,25 @@ def get_corpus(sentiment_type: str = "normal", maxlen=80):
     for msg, res in zip(corpus["X"], corpus["y"]):
         if len(msg) > maxlen or len(res) > maxlen:
             continue
+        if transform is not None:
+            msg = transform(msg)
+            res = transform(res)
         X.append(msg)
         y.append(res)
 
     return X, y
 
 
-def get_dataset(X, y, train_size=0.8, val_size=0.7, maxlen=140, transform=None):
-    from sklearn.model_selection import train_test_split
-    from dataloader.twitter_dataset import TwitterDataset
-
-    X_train, X_other, y_train, y_other = train_test_split(X, y, train_size=train_size)
-    X_val, X_test, y_val, y_test = train_test_split(X_other, y_other, train_size=val_size)
-
-    train_dataset = TwitterDataset(X_train, y_train, transform, maxlen)
-    val_dataset = TwitterDataset(X_val, y_val, transform, maxlen)
-    test_dataset = TwitterDataset(X_test, y_test, transform, maxlen)
-    all_dataset = [train_dataset, val_dataset, test_dataset]
-
-    return all_dataset
-
-
 def get_dataloader(all_dataset, batch_size=100, num_workers=52, pin_memory=True):
+    """ 
+    all_dataset:List[TwitterDataset]を受け取り，all_dataloader:List[DataLoader]を返す．
+    all_datasetは，train/val/testの順で作成されたTwitterDataset型のインスタンスを持つリスト． 
+    - パラメーター\n
+    all_dataset : [train_dataset, val_dataset, test_dataset]
+    - 戻り値\n
+    all_dataloader : [train_dataloader, val_dataloader, test_dataloader]
+    """
+
     from torch.utils.data import DataLoader
 
     train_dataloader = DataLoader(
@@ -40,7 +42,7 @@ def get_dataloader(all_dataset, batch_size=100, num_workers=52, pin_memory=True)
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        pin_memory=pin_memory,
+        # pin_memory=pin_memory,
         collate_fn=all_dataset[0].collate_fn,
     )
     val_dataloader = DataLoader(
@@ -48,7 +50,7 @@ def get_dataloader(all_dataset, batch_size=100, num_workers=52, pin_memory=True)
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        pin_memory=pin_memory,
+        # pin_memory=pin_memory,
         collate_fn=all_dataset[1].collate_fn,
     )
     test_dataloader = DataLoader(
@@ -56,38 +58,36 @@ def get_dataloader(all_dataset, batch_size=100, num_workers=52, pin_memory=True)
         batch_size=1,
         shuffle=False,
         num_workers=num_workers,
-        pin_memory=pin_memory,
+        # pin_memory=pin_memory,
         collate_fn=all_dataset[2].collate_fn,
     )
     all_dataloader = [train_dataloader, val_dataloader, test_dataloader]
     return all_dataloader
 
 
-def get_dataloader_pipeline(
+def __get_dataloader_pipline_no_load(
     vocab: TwitterVocab,
     sentiment_type: str = "normal",
     maxlen=80,
     train_size=0.8,
     val_size=0.7,
     transform=None,
-    batch_size=100,
-    num_workers=52,
     verbose=False,
+    is_saved=False,
 ):
+    def display_Xy(X, y, data_label: str, count=4):
+        print(f"check {data_label} data :")
+        for idx, (msg, res) in enumerate(zip(X, y)):
+            print(msg)
+            print(f"\t->{msg}")
+            if idx >= count:
+                break
+
     from sklearn.model_selection import train_test_split
     from dataloader.twitter_dataset import TwitterDataset
 
-    # コーパスを取得
-    messages, responses = get_corpus(sentiment_type, maxlen)
-
-    # 前処理
-    new_msg, new_res = [], []
-    if transform is not None:
-        for x, y in zip(messages, responses):
-            new_msg.append(x)
-            new_res.append(y)
-        messages = new_msg
-        responses = new_res
+    # 発話／応答リストを取得
+    messages, responses = get_corpus(sentiment_type, maxlen, transform)
 
     # データセットの分割
     X_train, X_train_other, y_train, y_train_other = train_test_split(
@@ -96,6 +96,12 @@ def get_dataloader_pipeline(
     X_val, X_test, y_val, y_test = train_test_split(
         X_train_other, y_train_other, train_size=val_size
     )
+
+    # データの中身を確認
+    if verbose:
+        display_Xy(X_train, y_train, "train")
+        display_Xy(X_val, y_val, "val")
+        display_Xy(X_test, y_test, "test")
 
     # テキストをベクトル化
     X_train = vocab.vocab_X.transform(X_train, is_wakati=False, verbose=verbose)
@@ -111,7 +117,66 @@ def get_dataloader_pipeline(
     test_dataset = TwitterDataset(X_test, y_test, maxlen)
     all_dataset = [train_dataset, val_dataset, test_dataset]
 
+    if is_saved:
+        train_dataset.save_dataset_pkl("train_dataset.pkl")
+        val_dataset.save_dataset_pkl("val_dataset.pkl")
+        test_dataset.save_dataset_pkl("test_dataset.pkl")
+
+    return all_dataset
+
+
+def __get_dataloader_pipline_load():
+    from dataloader.twitter_dataset import TwitterDataset
+
+    data_labels = ["train", "val", "test"]
+    all_datasets = []
+
+    for label in data_labels:
+        dataset = TwitterDataset()
+        dataset.load_dataset_pkl(f"{label}_dataset.pkl")
+        all_datasets.append(dataset)
+
+    return all_datasets
+
+
+def get_dataloader_pipeline(
+    vocab: TwitterVocab,
+    sentiment_type: str = "normal",
+    maxlen=80,
+    train_size=0.8,
+    val_size=0.7,
+    transform=None,
+    batch_size=100,
+    num_workers=52,
+    verbose=False,
+    is_saved=False,
+    is_load=False,
+):
+    """dataloaderを一括で受け取る．
+    
+    Parameter
+    ----------------------
+    - vocab : TwitterVocab 辞書データロード済みの語彙マップクラス
+    - sentiment_type: str 利用するコーパスタイプ
+    - maxlen: int 最大系列数（文字数）．maxlen+1以上の文字数を持つ対話ターンは削減する．
+    - train_size: float 
+    - val_size: float
+    - transform: 前処理クラス
+    - batch_size: int バッチサイズ
+    - num_workers
+    - verbose
+    - is_saved: bool ロードしたデータセットのpklデータをセーブする
+    - is_load: bool データセットのpklをロードする．
+    """
+
+    if is_load:
+        all_dataset = __get_dataloader_pipline_load()
+    else:
+        all_dataset = __get_dataloader_pipline_no_load(
+            vocab, sentiment_type, maxlen, train_size, val_size, transform, verbose, is_saved
+        )
+
     # DataLoaderの作成
-    all_dataloader = get_dataloader(all_dataset,batch_size=batch_size, num_workers=num_workers)
+    all_dataloader = get_dataloader(all_dataset, batch_size=batch_size, num_workers=num_workers)
 
     return all_dataloader
